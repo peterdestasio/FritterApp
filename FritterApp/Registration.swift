b39bd53eb9c158b569089c152c8e345311524887
Pierluigi De Stasio <peterdestasio@gmail.com>
2017-07-17 22:10:17 -0400

    logiic login

--- FritterApp/Registration.swift
+++ FritterApp/Registration.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class Registration: UIViewController {
 
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var creditCardNumberTextField: UITextField!
+    @IBOutlet weak var ccvTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +40,54 @@ class Registration: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func signUp(_ sender: Any) {
+        
+        let userEmail = userEmailTextField.text
+        let userPassword = userPasswordTextField.text
+        //let firstName = firstNameTextField.text
+        //let lastName = lastNameTextField.text
+        //let birthday = birthdayTextField.text
+        //let city = cityTextField.text
+        //let creditCardNumber = creditCardNumberTextField.text
+        //let ccv = ccvTextField.text
+        
+        //check for empty field
+        
+        if((userEmail?.isEmpty)! || (userPassword?.isEmpty)!){
+            
+            //display alert message
+            
+           displayMyAlertMessage(userMessage: "You should insert at least user and password")
+            return;
+            
+        }
+        
+        //store data
+        UserDefaults.standard.set(userEmail, forKey: value(forKey: "userEmail") as! String)
+        UserDefaults.standard.set(userPassword, forKey: value(forKey: "userPassword") as! String)
+        UserDefaults.standard.synchronize();
+        
+        
+        //display alert message with confirmation
+         let myAlert = UIAlertController(title: "Alert", message: "Registration Successful", preferredStyle: UIAlertControllerStyle.alert)
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil);
+        
+        myAlert.addAction(okAction)
+        
+    }
+    
+    
+    func displayMyAlertMessage(userMessage: String){
+        
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil);
+        
+        myAlert.addAction(okAction)
+        
+        
+        self.present(myAlert, animated: true, completion: nil)
+        
+    }
 
 }
--- FritterApp/ViewController.swift
+++ FritterApp/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func login(_ sender: Any) {
+        let userEmail = userEmailTextField.text
+        let userPassword = userPasswordTextField.text
+        
+        //if userEmil == userEmail stored && userPwd = pwdStored do
+        
+    }
 
 }
 
