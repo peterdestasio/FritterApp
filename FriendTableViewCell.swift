f9352c19fc0dadd61948eb49b65e6229391318e4
Pierluigi De Stasio <peterdestasio@gmail.com>
2017-08-08 22:44:44 -0400

    following1

---
+++ FriendTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FriendTableViewCell.swift
+//  FritterApp
+//
+//  Created by Pierluigi De Stasio on 2017-08-08.
+//  Copyright © 2017 Peter. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var labelName: UILabel!
+ 
+    @IBOutlet weak var labelSur: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FritterApp/SearchFriend.swift
+++ FritterApp/SearchFriend.swift
@@ -8,10 +8,83 @@
 
 import UIKit
 
-class SearchFriend: UIViewController {
+class SearchFriend: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    let animals = ["panda", "lion", "elephant"]
+    
+    var emails = [String]()
+    var passwords = [String]()
+    var userIDs = [String]()
+    var userNames = [String]()
+    var lastNames = [String]()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        return userNames.count
+    }
+    
+    
+   
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FriendTableViewCell
+        
+        cell.labelName.text = userNames[indexPath.row]
+        cell.labelSur.text = lastNames[indexPath.row]
+        
+        return (cell)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+            
+            let url = URL(string:"http://localhost/fritter/serviceselect.php")
+            
+            do {
+                let data = try Data(contentsOf: url!) // received from a network request, for example
+                
+                
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! Array<AnyObject>
+                
+                //print(json[0]["user_email"])
+                
+                for item in json { // loop through data items
+                    let obj = item as! NSDictionary
+                    for (key, value) in obj {
+                        if((key as! String) == "user_id")
+                        {
+                            userIDs.append(value as! String)
+                        }
+                        if((key as! String) == "first_name")
+                        {
+                            userNames.append(value as! String)
+                        }
+                        if((key as! String) == "last_name")
+                        {
+                            lastNames.append(value as! String)
+                        }
+                        if((key as! String) == "user_pass")
+                        {
+                            passwords.append(value as! String)
+                        }
+                        if((key as! String) == "user_email")
+                        {
+                            emails.append(value as! String)
+                        }
+                    }
+                }
+                print(emails)
+                print(passwords)
+                
+                
+            }
+            catch {
+                print("Error deserialization")
+            }
 
         // Do any additional setup after loading the view.
     }
